22c2d3da2027684b1f48c9a5404da46a1b779d07
lsc2159 <127822509+lsc2159@users.noreply.github.com>
2023-05-14 20:05:31 +0900

    Update 2480.py

--- 2480.py
+++ 2480.py
@@ -1,10 +1,4 @@
-import random
-
-dice1=random.randint(1,6)
-dice2=random.randint(1,6)
-dice3=random.randint(1,6)
-
-print(dice1, dice2, dice3)
+dice1, dice2, dice3=map(int,input().split()) #map 함수 찾아봄요 ㅈㅅㅈㅅ
 
 if dice1==dice2==dice3:
     prize1=10000+(dice1*1000)
@@ -22,7 +16,7 @@ if dice2==dice3!=dice1:
     prize4=1000+(dice2*100) 
     print(prize4)   
         
-if dice1!=dice2!=dice3:
+if dice1!=dice2 and dice2!=dice3 and dice1!=dice3:
     if dice1>dice2 and dice1>dice3:
         print(dice1*100)
     if dice2>dice1 and dice2>dice3:
