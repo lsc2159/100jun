a0f30fe87ffec8a89a684ecf001feda24494b99c
lsc2159 <127822509+lsc2159@users.noreply.github.com>
2023-05-14 20:27:34 +0900

    11720

--- 11720.py
+++ 11720.py
@@ -1,10 +1,6 @@
-N=int(input('N 숫자 입력: '))
-if 1<=N<=100:
-    print(N)
-    a=input()
-    sum=0
-    for i in range(N):
-        sum+=int(a[i])
-    print(sum)
-
-
+N=int(input())
+a=input()
+sum=0
+for i in range(N):
+    sum+=int(a[i])
+print(sum)
